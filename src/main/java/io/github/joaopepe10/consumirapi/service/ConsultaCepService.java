ec2d36595ee71aaadd265e5119d7d934cde650e6
João Perdro Pires <joaopedropires61@gmail.com>
2023-12-31 17:13:13 -0300

    feat: Altera validação e retorno apropriado

--- src/main/java/io/github/joaopepe10/consumirapi/service/ConsultaCepService.java
+++ src/main/java/io/github/joaopepe10/consumirapi/service/ConsultaCepService.java
@@ -3,12 +3,13 @@ package io.github.joaopepe10.consumirapi.service;
 import io.github.joaopepe10.consumirapi.exception.CepException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ConsultaCepService {
     public String validaCep(String cep) throws IllegalAccessException {
-        if (cep == null || cep.length() != 8){
-            throw new CepException("CEP INVÁLIDO");
-        }
-       return cep;
+       return Optional.ofNullable(cep)
+               .filter(c -> c.length() == 8)
+               .orElseThrow(() -> new CepException("Cep invalido"));
     }
 }
