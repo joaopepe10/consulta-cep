5881c463c2042d08cb2e8a6f9f1e0089dbd1e6da
João Perdro Pires <joaopedropires61@gmail.com>
2023-12-31 15:23:15 -0300

    refactor: faz validação do cep na camada service

--- src/main/java/io/github/joaopepe10/consumirapi/controller/ConsultaCepAPI.java
+++ src/main/java/io/github/joaopepe10/consumirapi/controller/ConsultaCepAPI.java
@@ -1,6 +1,9 @@
 package io.github.joaopepe10.consumirapi.controller;
 
 import io.github.joaopepe10.consumirapi.dto.CepResultDTO;
+import io.github.joaopepe10.consumirapi.service.ConsultaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +14,12 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("consulta-cep")
 public class ConsultaCepAPI {
+    @Autowired
+    private ConsultaCepService consultaCepService;
     @GetMapping("{cep}")
-    public CepResultDTO consultaCep(@PathVariable("cep") String cep) {
+    public CepResultDTO consultaCep(@PathVariable("cep") String cep) throws IllegalAccessException {
         RestTemplate restTemplate = new RestTemplate();
+        cep = consultaCepService.validaCep(cep);
         String url = "https://viacep.com.br/ws/" + cep + "/json/";
         ResponseEntity<CepResultDTO> response = restTemplate.getForEntity(url, CepResultDTO.class);
         return response.getBody();
