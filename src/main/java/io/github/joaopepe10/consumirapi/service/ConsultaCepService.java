2531b845baee75166cee32e8ce38708a30371c2f
João Perdro Pires <joaopedropires61@gmail.com>
2023-12-31 15:32:42 -0300

    refactor: Altera exceção lançada

--- src/main/java/io/github/joaopepe10/consumirapi/service/ConsultaCepService.java
+++ src/main/java/io/github/joaopepe10/consumirapi/service/ConsultaCepService.java
@@ -1,12 +1,13 @@
 package io.github.joaopepe10.consumirapi.service;
 
+import io.github.joaopepe10.consumirapi.exception.CepException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsultaCepService {
     public String validaCep(String cep) throws IllegalAccessException {
         if (cep == null || cep.length() != 8){
-            throw new E("CEP INVÁLIDO");
+            throw new CepException("CEP INVÁLIDO");
         }
        return cep;
     }
