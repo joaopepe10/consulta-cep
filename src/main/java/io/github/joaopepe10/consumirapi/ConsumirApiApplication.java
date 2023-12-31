613ce3a9cf0c330d2c9be8170d8c7fae82d8dd24
João Perdro Pires <joaopedropires61@gmail.com>
2023-12-31 14:04:29 -0300

    Inicializa projeto Spring Web

---
+++ src/main/java/io/github/joaopepe10/consumirapi/ConsumirApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaopepe10.consumirapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumirApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumirApiApplication.class, args);
+	}
+
+}
