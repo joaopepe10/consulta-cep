a2fc4fa12524275899194c3e2ecd2b1cea9bbf6f
João Perdro Pires <joaopedropires61@gmail.com>
2023-12-31 14:05:38 -0300

    Inicializa projeto Spring Web

---
+++ src/test/java/io/github/joaopepe10/consumirapi/ConsumirApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaopepe10.consumirapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumirApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
